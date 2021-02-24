ba69df53a6c88b406aea2e421107c26a54335f3c
Evaldas <doorsas@gmail.com>
2021-02-24 09:50:46 +0200

    21 pataisymas

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Rustus Gidas"
+admin.site.site_title = "Rustus Gidas Gi"
+admin.site.index_title = "Rustus Gidas GID"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
